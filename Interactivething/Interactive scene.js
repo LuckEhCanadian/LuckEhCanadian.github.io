2fc7c36b3e4ca21a8d53e085367a846b224212d3
LuckEhCanadian <aidenflames03@gmail.com>
2019-09-11 14:11:21 -0600

    Everything is broken

---
+++ Interactivething/Interactive scene.js
@@ -0,0 +1,25 @@
+// Line art demo
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw(){
+}
+
+function drop_ball(){
+  if (keyIsPressed){
+    console.log("not here")
+    if (key === 'b'){
+      fill(255,0,255)
+      ellipse(mouseX, mouseY, 35, [h])
+    }
+  }
+}

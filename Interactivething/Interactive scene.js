58b42389cc25ab75096fbff0984f50b38ff98e57
LuckEhCanadian <aidenflames03@gmail.com>
2019-09-12 12:23:19 -0600

    yikes

--- Interactivething/Interactive scene.js
+++ Interactivething/Interactive scene.js
@@ -6,20 +6,18 @@
 // - describe what you did to take this project "above and beyond"
 
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
 }
 
 function draw(){
-}
+  fill(0,0,255);
+  ellipse(300, (300), 75, [h]);
+  fill(255);
+  ellipse(675, (windowHeight/3),65, [h]);
+  fill(255,0,0);
+  ellipse(675, (windowHeight/3), 15, [h]);
 
-function drop_ball(){
-  if (keyIsPressed){
-    console.log("not here")
-    if (key === 'b'){
-      fill(255,0,255)
-      ellipse(mouseX, mouseY, 35, [h])
-    }
-  }
 }

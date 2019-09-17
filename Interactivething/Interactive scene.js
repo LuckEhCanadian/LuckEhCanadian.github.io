42c23a659f578a867ce0f330b8b0e976e5a69fe0
LuckEhCanadian <aidenflames03@gmail.com>
2019-09-17 14:10:56 -0600

    emp nade added
    still trying to fix all the bugs with the nade before adding winning and losing

--- Interactivething/Interactive scene.js
+++ Interactivething/Interactive scene.js
@@ -7,6 +7,8 @@
 
 let thatcher;
 let maverick;
+let emp;
+let consulate;
 
 let mavY;
 let mavX;
@@ -14,9 +16,15 @@ let mavX;
 let thatY;
 let thatX;
 
+let empX;
+let empY;
+let empDX;
+
 function preload(){
   thatcher = loadImage("assets/deletthis.png");
   maverick = loadImage("assets/Mavchibi.png");
+  emp = loadImage('assets/empnade.png');
+  consulate = loadImage('assets/consulate.jpg')
 }
 
 
@@ -27,15 +35,19 @@ function setup() {
   mavX = ((windowWidth/6*5));
   thatY = (windowHeight/2);
   thatX = ((windowHeight/6));
+  empX = thatX;
+  empY = thatY;
+  empDX = 10;
 
   background(255);
 }
 
 function draw(){
-  background(255);
+  background(consulate);
   moveMav();
-  //thatcherMove();
+  thatcherMove();
   image(maverick, mavX, mavY, 150, 150);
+  image(emp, empX, empY, 90, 50)
   image(thatcher, thatX, thatY, 150, 175);
 }
 
@@ -51,12 +63,42 @@ function moveMav(){
   else if (mavY > 0){
     (mavY -= 5);
   }
-  else if ((mavY +150) < height){
+  else if ((mavY + 150) < height){
     (mavY += 5);
+  }
+  if (mavX > 0 && (mavX + 150) < width){
+    if (keyIsDown(65)){
+      (mavX -= 5);
+    }
+    else if (keyIsDown(68)){
+      (mavX += 5)
+    }
+  }
+  else if (mavX > 0){
+    (mavX -= 5);
+  }
+  else if ((mavX + 150) < height){
+    (mavX += 5);
   }
 }
 function thatcherMove(){
-  if (thatY > 0 && (thatY + 175) > 0){
-    (thatY += random(-20,20))
+  if (thatY === mavY){
+    launch();
+  }
+  else if (thatY < mavY){
+    (thatY += 2.5);
+  }
+  else if (thatY > mavY){
+    (thatY -= 2.5);
+  }
+}
+
+function launch(){
+  if (empX < width){
+    (empX += empDX);
+  }
+  else{
+    (empX = thatX);
+    (empY = (thatY+75));
   }
 }

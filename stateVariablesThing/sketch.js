05b38178a5700bc83285296c7c7783f4418fa27d
LuckEhCanadian <aidenflames03@gmail.com>
2019-10-02 14:12:15 -0600

    juke box started

--- stateVariablesThing/sketch.js
+++ stateVariablesThing/sketch.js
@@ -1,11 +1,10 @@
-// Project Title
-// Your Name
+// State variables project
+// Aiden Jorgensen
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let redamount = 150;
-let redchangeamount = 1;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,17 +12,16 @@ function setup() {
 }
 
 function draw() {
-  fill(redamount, 0, 255);
-  noStroke();
-  ellipse(mouseX,mouseY,100,100);
-
-  redamount+=redchangeamount;
-
-  if (redamount >= 255){
-     redchangeamount *= -1;
-  }
-  if (redamount <= 0){
-    redchangeamount *= -1
-  }
-  console.log(redamount);
+  rectMode(CENTER)
+  background(0, 0, 0);
+  fill(187,44,16);
+  ellipse(windowWidth/2, windowHeight- (windowHeight/2), windowWidth/3);
+  rect(windowWidth/2, windowHeight - windowHeight/4, windowWidth/3, windowHeight/2);
+  fill(249,123,12);
+  arc(windowWidth/2, windowHeight- (windowHeight/2), windowWidth/3.5, windowWidth/3.5, PI, 0, open);
+  rect(windowWidth/2, windowHeight - windowHeight/2.5, windowWidth/5, windowHeight/30,);
+  rect(windowWidth/2, windowHeight - windowHeight/2.85, windowWidth/5, windowHeight/15);
+  fill(187,44,16);
+  rect(windowWidth/2 - windowWidth/6.5, windowHeight - windowHeight/4, windowWidth/30, windowHeight/2);
+  rect(windowWidth/2 + windowWidth/6.5, windowHeight - windowHeight/4, windowWidth/30, windowHeight/2);
 }

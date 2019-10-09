8843d596b7c007e795b358ab4b9e9d58fc7ecea7
LuckEhCanadian <aidenflames03@gmail.com>
2019-10-09 14:11:39 -0600

    Links continued

--- stateVariablesThing/sketch.js
+++ stateVariablesThing/sketch.js
@@ -22,7 +22,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0, 0, 0);
   textFont(font);
-  textSize(36)
+  textSize(36);
 }
 
 function draw() {
@@ -33,24 +33,138 @@ function draw() {
     displayGenres();
   }
   else if(state === 'classicRock'){
+    textSize(26);
     displayClassic();
   }
   else if(state === 'punkRock'){
-    diplayPunk();
+    textSize(26);
+    displayPunk();
   }
   else if(state === 'popIsh'){
+    textSize(26);
     displayPopIsh();
   }
-  else if (state === 'newRock'){
-    diplayNew();
+  else if (state === 'altRock'){
+    textSize(26);
+    displayAlt();
   }
   else if (state ==='memes'){
+    textSize(26);
     displayMemes();
   }
   else if (state === 'oddBalls'){
+    textSize(26);
     displayOdd();
   }
 }
+
+function keyPressed() {
+  if (state === "genreSelect") {
+    if (keyCode === 49 ||keyCode === 35){
+      state = "classicRock";
+    }
+    else if (keyCode === 50 ||keyCode === 40){
+      state = "altRock";
+    }
+    else if (keyCode === 51 ||keyCode === 34){
+      state = "punkRock";
+    }
+    else if (keyCode === 52 ||keyCode === 37){
+      state = "oddBalls"
+    }
+    else if (keyCode === 53 ||keyCode === 12){
+      state = "memes"
+    }
+    else if (keyCode === 54 ||keyCode === 39){
+      state = "popIsh"
+    }
+  }
+  else if(state === "classicRock"){
+    if (keyCode === 49 ||keyCode === 35){
+        window.open('https://www.youtube.com/watch?v=y8OtzJtp-EM');
+    }
+    else if (keyCode === 50 ||keyCode === 40){
+      window.open('https://www.youtube.com/watch?v=qBdFA6sI6-8');
+    }
+    else if (keyCode === 51 ||keyCode === 34){
+      window.open('https://www.youtube.com/watch?v=T0ZmErXkbxE');
+    }
+    else if (keyCode === 52 ||keyCode === 37){
+      window.open('https://www.youtube.com/watch?v=MhkPWV97GQU');
+    }
+    else if (keyCode === 53 ||keyCode === 12){
+      window.open('https://www.youtube.com/watch?v=fKhTk0IynHM');
+    }
+    else if (keyCode === 54 ||keyCode === 39){
+      window.open('https://www.youtube.com/watch?v=EJRdDhnTRoo');
+      
+    }
+  }
+  else if (state === "punkRock"){
+    if (keyCode === 49 ||keyCode === 35){
+      window.open('https://www.youtube.com/watch?v=iymtpePP8I8');
+    }
+    else if (keyCode === 50 ||keyCode === 40){
+      window.open('https://www.youtube.com/watch?v=z7hhDINyBP0');
+    }
+    else if (keyCode === 51 ||keyCode === 34){
+      window.open('https://www.youtube.com/watch?v=kJsNeFdEBmU');
+    }
+    else if (keyCode === 52 ||keyCode === 37){
+      window.open('https://www.youtube.com/watch?v=tuK6n2Lkza0');
+    }
+    else if (keyCode === 53 ||keyCode === 12){
+      window.open('https://www.youtube.com/watch?v=hTWKbfoikeg');
+    }
+    else if (keyCode === 54 ||keyCode === 39){
+      window.open('https://www.youtube.com/watch?v=JoC3PUBmhFs');
+      
+    }
+  }
+  else if (state === "altRock"){
+    if (keyCode === 49 ||keyCode === 35){
+      window.open('https://www.youtube.com/watch?v=I7rCNiiNPxA');
+    }
+    else if (keyCode === 50 ||keyCode === 40){
+      window.open('https://www.youtube.com/watch?v=z7hhDINyBP0');
+    }
+    else if (keyCode === 51 ||keyCode === 34){
+      window.open('https://www.youtube.com/watch?v=ENXvZ9YRjbo');
+    }
+    else if (keyCode === 52 ||keyCode === 37){
+      window.open('https://www.youtube.com/watch?v=mzJj5-lubeM');
+    }
+    else if (keyCode === 53 ||keyCode === 12){
+      window.open('https://www.youtube.com/watch?v=LAZUsCONjIQ');
+    }
+    else if (keyCode === 54 ||keyCode === 39){
+      window.open('https://www.youtube.com/watch?v=XFkzRNyygfk');
+      
+    }
+  }
+  else if(state === "popIsh"){
+    if (keyCode === 49 ||keyCode === 35){
+      window.open('https://www.youtube.com/watch?v=I7rCNiiNPxA');
+    }
+    else if (keyCode === 50 ||keyCode === 40){
+      window.open('https://www.youtube.com/watch?v=z7hhDINyBP0');
+    }
+    else if (keyCode === 51 ||keyCode === 34){
+      window.open('https://www.youtube.com/watch?v=ENXvZ9YRjbo');
+    }
+    else if (keyCode === 52 ||keyCode === 37){
+      window.open('https://www.youtube.com/watch?v=LAZUsCONjIQ');
+    }
+    else if (keyCode === 53 ||keyCode === 12){
+      window.open('https://www.youtube.com/watch?v=mzJj5-lubeM');
+    }
+    else if (keyCode === 54 ||keyCode === 39){
+      window.open('https://www.youtube.com/watch?v=XFkzRNyygfk');
+      
+    }
+  }
+}
+
 
 function displayBox(){
   rectMode(CENTER)
@@ -77,31 +191,11 @@ function displayBox(){
 function displayGenres(){
   optionA = "1.Classic Rock";
   optionB = "3.Punk Rock";
-  optionC = "4.New Rock";
-  optionD = "2.Oddballs";
-  optionE = '5.Memes'
+  optionC = '5.Memes'
+  optionD = "2.Alternative Rock";
+  optionE = "4.Oddballs";
   optionF = '6.Pop-ish'
   displayMain();
-  if (keyIsPressed){
-    if (keyCode === 49 ||keyCode === 35){
-        state = "classicRock";
-    }
-    else if (keyCode === 50 ||keyCode === 40){
-      state = "oddBalls";
-    }
-    else if (keyCode === 51 ||keyCode === 34){
-      state = "punkRock";
-    }
-    else if (keyCode === 52 ||keyCode === 37){
-      state = "newRock"
-    }
-    else if (keyCode === 53 ||keyCode === 12){
-      state = "memes"
-    }
-    else if (keyCode === 54 ||keyCode === 39){
-      state = "popIsh"
-    }
-  }
 }
 
 function displayMain(){
@@ -128,31 +222,40 @@ function displayMain(){
 function displayClassic(){
   optionA = "1.Immigrant Song - Led Zepplin";
   optionB = "3.Paradice City - Guns & Roses";
-  optionC = "4.For Those About To Rock - ACDC";
+  optionC = "5.For Those About To Rock - ACDC";
   optionD = "2.Fox On The Run - Sweet";
-  optionE = '5.Bohemian Rhasody - Queen'
+  optionE = '4.Bohemian Rhasody - Queen'
   optionF = '6.Gimme Shelter - The Rolling Stones'
   displayMain();
-  wait(1500);
-  if (keyIsPressed){
-    if (keyCode === 49 ||keyCode === 35){
-        window.open('https://www.youtube.com/watch?v=y8OtzJtp-EM');
-    }
-    else if (keyCode === 50 ||keyCode === 40){
-      window.open('https://www.youtube.com/watch?v=qBdFA6sI6-8');
-    }
-    else if (keyCode === 51 ||keyCode === 34){
-      window.open('https://www.youtube.com/watch?v=T0ZmErXkbxE');
-    }
-    else if (keyCode === 52 ||keyCode === 37){
-      window.open('https://www.youtube.com/watch?v=fKhTk0IynHM');
-    }
-    else if (keyCode === 53 ||keyCode === 12){
-      window.open('https://www.youtube.com/watch?v=MhkPWV97GQU');
-    }
-    else if (keyCode === 54 ||keyCode === 39){
-      window.open('https://www.youtube.com/watch?v=EJRdDhnTRoo')
-      
-    }
-  }
+  
+}
+
+function displayPunk(){
+  optionA = "1.Blitzkrieg Bop - Ramones";
+  optionB = "3.Holiday/Boulevard Of Broken Dreams - Green Day";
+  optionC = "5.Smells Like Teen Spirit - Nirvana";
+  optionD = "2.The Rock Show - Blink 182";
+  optionE = '4.Are You Gonna Be My Girl - Jet'
+  optionF = '6.In To Deep - Sum 41'
+  displayMain();
+}
+
+function displayAlt(){
+  optionA = "1.Monkey Wrench - Foo Fighters";
+  optionB = "3.Say It Ain't So - Weezer";
+  optionC = "5.New Orleans Is Sinking - The Tragically Hip";
+  optionD = "2.The Rock Show - Blink 182";
+  optionE = '4.Scar Tissue - Red Hot Chilli Peppers'
+  optionF = '6.Creep - Radio Head'
+  displayMain();
+}
+//haven't done popish links yet
+function displayPopIsh(){
+  optionA = "1.Come And Get Your Love - Redbone";
+  optionB = "3.Mr. Blue Sky - Electric Light Orchestra";
+  optionC = "5.New Orleans Is Sinking - The Tragically Hip";
+  optionD = "2.Happier - Marshmello";
+  optionE = '4.'
+  optionF = '6.Creep - Radio Head'
+  displayMain();
 }

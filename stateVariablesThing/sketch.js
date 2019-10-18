41e0a85f24b4f2c18c8039342968451f7d140cca
LuckEhCanadian <aidenflames03@gmail.com>
2019-10-18 14:11:27 -0600

    pretty muched finished the juke box

--- stateVariablesThing/sketch.js
+++ stateVariablesThing/sketch.js
@@ -1,10 +1,12 @@
 // State variables project
 // Aiden Jorgensen
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Monday October 24 2019
+//the state varible is used to check what page is showing
 
+// Extra for Experts:
+// - not sure if it counts but using links to websites wasn't something we did in class and neither was using fonts in Javascript
+
+//this is where i declare all my variables i'm going to use later
 state = "start"
 let optionA;
 let optionB;
@@ -15,10 +17,12 @@ let optionF;
 let font;
 let songHistory = [];
 
+//the only thing i'm pre loading is the font
 function preload(){
   font = loadFont('assets/rock_font.ttf');
 }
 
+//creating the canvas, seting text size and font
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0, 0, 0);
@@ -26,6 +30,7 @@ function setup() {
   textSize(36);
 }
 
+//where all my main functions are called
 function draw() {
   if (state === "start"){
     displayBox();
@@ -53,12 +58,10 @@ function draw() {
     textSize(26);
     displayMemes();
   }
-  else if (state === 'oddBalls'){
-    textSize(26);
-    displayHistory();
-  }
 }
 
+//this mass of code is mainly links to YouTube videos of all the songs i'm playing
+//and making sure they're being added to song history(song history is in the console after you press 6.)
 function keyPressed() {
   if (state === "genreSelect") {
     if (keyCode === 49 ||keyCode === 35){
@@ -80,94 +83,151 @@ function keyPressed() {
       state = "history"
     }
   }
+  //all the classic rock links
   else if(state === "classicRock"){
     if (keyCode === 49 ||keyCode === 35){
         window.open('https://www.youtube.com/watch?v=y8OtzJtp-EM');
-        songHistory.push('Immigrant Song - Led Zepplin');
+        songHistory.push(optionA);
     }
     else if (keyCode === 50 ||keyCode === 40){
       window.open('https://www.youtube.com/watch?v=qBdFA6sI6-8');
+      songHistory.push(optionD);
     }
     else if (keyCode === 51 ||keyCode === 34){
       window.open('https://www.youtube.com/watch?v=T0ZmErXkbxE');
+      songHistory.push(optionB);
     }
     else if (keyCode === 52 ||keyCode === 37){
       window.open('https://www.youtube.com/watch?v=MhkPWV97GQU');
+      songHistory.push(optionE);
     }
     else if (keyCode === 53 ||keyCode === 12){
       window.open('https://www.youtube.com/watch?v=fKhTk0IynHM');
+      songHistory.push(optionC);
     }
     else if (keyCode === 54 ||keyCode === 39){
       window.open('https://www.youtube.com/watch?v=EJRdDhnTRoo');
+      songHistory.push(optionF);
     }
     else if (keyCode === 27){
       state = 'genreSelect'
     }
   }
+  //all the puck rock links
   else if (state === "punkRock"){
     if (keyCode === 49 ||keyCode === 35){
       window.open('https://www.youtube.com/watch?v=iymtpePP8I8');
+      songHistory.push(optionA);
     }
     else if (keyCode === 50 ||keyCode === 40){
       window.open('https://www.youtube.com/watch?v=z7hhDINyBP0');
+      songHistory.push(optionD);
     }
     else if (keyCode === 51 ||keyCode === 34){
       window.open('https://www.youtube.com/watch?v=kJsNeFdEBmU');
+      songHistory.push(optionB);
     }
     else if (keyCode === 52 ||keyCode === 37){
       window.open('https://www.youtube.com/watch?v=tuK6n2Lkza0');
+      songHistory.push(optionE);
     }
     else if (keyCode === 53 ||keyCode === 12){
       window.open('https://www.youtube.com/watch?v=hTWKbfoikeg');
+      songHistory.push(optionC);
     }
     else if (keyCode === 54 ||keyCode === 39){
       window.open('https://www.youtube.com/watch?v=JoC3PUBmhFs'); 
+      songHistory.push(optionF);
     }
     else if (keyCode === 27){
       state = 'genreSelect'
     }
   }
+  //all the Alt-rock links
   else if (state === "altRock"){
     if (keyCode === 49 ||keyCode === 35){
       window.open('https://www.youtube.com/watch?v=I7rCNiiNPxA');
+      songHistory.push(optionA);
     }
     else if (keyCode === 50 ||keyCode === 40){
       window.open('https://www.youtube.com/watch?v=z7hhDINyBP0');
+      songHistory.push(optionD);
     }
     else if (keyCode === 51 ||keyCode === 34){
       window.open('https://www.youtube.com/watch?v=ENXvZ9YRjbo');
+      songHistory.push(optionB);
     }
     else if (keyCode === 52 ||keyCode === 37){
       window.open('https://www.youtube.com/watch?v=mzJj5-lubeM');
+      songHistory.push(optionE);
     }
     else if (keyCode === 53 ||keyCode === 12){
       window.open('https://www.youtube.com/watch?v=LAZUsCONjIQ');
+      songHistory.push(optionC);
     }
     else if (keyCode === 54 ||keyCode === 39){
       window.open('https://www.youtube.com/watch?v=XFkzRNyygfk'); 
+      songHistory.push(optionF);
     }
     else if (keyCode === 27){
       state = 'genreSelect'
     }
   }
+  //all the (kinda)pop links
   else if(state === "popIsh"){
     if (keyCode === 49 ||keyCode === 35){
-      window.open('https://www.youtube.com/watch?v=I7rCNiiNPxA');
+      window.open('https://www.youtube.com/watch?v=bc0KhhjJP98');
+      songHistory.push(optionA);
     }
     else if (keyCode === 50 ||keyCode === 40){
-      window.open('https://www.youtube.com/watch?v=z7hhDINyBP0');
+      window.open('https://www.youtube.com/watch?v=m7Bc3pLyij0');
+      songHistory.push(optionD);
     }
     else if (keyCode === 51 ||keyCode === 34){
-      window.open('https://www.youtube.com/watch?v=ENXvZ9YRjbo');
+      window.open('https://www.youtube.com/watch?v=G8dsvclf3Tk');
+      songHistory.push(optionB);
     }
     else if (keyCode === 52 ||keyCode === 37){
-      window.open('https://www.youtube.com/watch?v=LAZUsCONjIQ');
+      window.open('https://www.youtube.com/watch?v=s93NCuXt7x4');
+      songHistory.push(optionE);
     }
     else if (keyCode === 53 ||keyCode === 12){
-      window.open('https://www.youtube.com/watch?v=mzJj5-lubeM');
+      window.open('https://www.youtube.com/watch?v=Hy_xJRbTq2Q');
+      songHistory.push(optionC);
     }
     else if (keyCode === 54 ||keyCode === 39){
-      window.open('https://www.youtube.com/watch?v=XFkzRNyygfk');
+      window.open('https://www.youtube.com/watch?v=2K2Lo-vThho');
+      songHistory.push(optionF);
+    }
+    else if (keyCode === 27){
+      state = 'genreSelect'
+    }
+  }
+  //all the meme links
+  else if(state === "memes"){
+    if (keyCode === 49 ||keyCode === 35){
+      window.open('https://www.youtube.com/watch?v=E9s1ltPGQOo');
+      songHistory.push(optionA);
+    }
+    else if (keyCode === 50 ||keyCode === 40){
+      window.open('https://www.youtube.com/watch?v=CSvFpBOe8eY');
+      songHistory.push(optionD);
+    }
+    else if (keyCode === 51 ||keyCode === 34){
+      window.open('https://www.youtube.com/watch?v=SYnVYJDxu2Q');
+      songHistory.push(optionB);
+    }
+    else if (keyCode === 52 ||keyCode === 37){
+      window.open('https://www.youtube.com/watch?v=L_jWHffIx5E');
+      songHistory.push(optionE);
+    }
+    else if (keyCode === 53 ||keyCode === 12){
+      window.open('https://www.youtube.com/watch?v=q6-ZGAGcJrk');
+      songHistory.push(optionC);
+    }
+    else if (keyCode === 54 ||keyCode === 39){
+      window.open('https://www.youtube.com/watch?v=FTQbiNvZqaY');
+      songHistory.push(optionF);
       
     }
     else if (keyCode === 27){
@@ -175,13 +235,12 @@ function keyPressed() {
     }
   }
   else if(state === "history"){
-    if (keyCode === 27){
-      state = 'genreSelect'
-    }
+    console.log(songHistory);
+    state = 'genreSelect';
   }
 }
 
-
+//this is where i draw the Jukebox at the start of the code
 function displayBox(){
   rectMode(CENTER)
   background(0, 0, 0);
@@ -204,16 +263,7 @@ function displayBox(){
   }
 }
 
-function displayGenres(){
-  optionA = "1.Classic Rock";
-  optionB = "3.Punk Rock";
-  optionC = '5.Memes'
-  optionD = "2.Alternative Rock";
-  optionE = "4.Pop-ish";
-  optionF = "6.History";
-  displayMain();
-}
-
+//this is the menu I use everywhere
 function displayMain(){
   background (249,123,12);
   fill(187,44,16);
@@ -235,63 +285,74 @@ function displayMain(){
   text(optionF, (3.1* windowWidth/5), windowHeight/4 + 2*(windowHeight/4));
 }
 
+//the selection of genres avalible
+function displayGenres(){
+  optionA = "1.Classic Rock";
+  optionB = "3.Punk Rock";
+  optionC = '5.Memes'
+  optionD = "2.Alternative Rock";
+  optionE = "4.Pop-ish";
+  optionF = "6.History";
+  displayMain();
+}
+
+//selections of classic songs availible
 function displayClassic(){
   optionA = "1.Immigrant Song - Led Zepplin";
   optionB = "3.Paradice City - Guns & Roses";
   optionC = "5.For Those About To Rock - ACDC";
   optionD = "2.Fox On The Run - Sweet";
-  optionE = '4.Bohemian Rhasody - Queen'
-  optionF = '6.Gimme Shelter - The Rolling Stones'
+  optionE = '4.Bohemian Rhasody - Queen';
+  optionF = '6.Gimme Shelter - The Rolling Stones';
   displayMain();
   
 }
 
+//selection of punk rock songs availible
 function displayPunk(){
   optionA = "1.Blitzkrieg Bop - Ramones";
   optionB = "3.Holiday/Boulevard Of Broken Dreams - Green Day";
   optionC = "5.Smells Like Teen Spirit - Nirvana";
   optionD = "2.The Rock Show - Blink 182";
-  optionE = '4.Are You Gonna Be My Girl - Jet'
-  optionF = '6.In To Deep - Sum 41'
+  optionE = '4.Are You Gonna Be My Girl - Jet';
+  optionF = '6.In To Deep - Sum 41';
   displayMain();
 }
 
+//selection of alternative rock availible
 function displayAlt(){
   optionA = "1.Monkey Wrench - Foo Fighters";
   optionB = "3.Say It Ain't So - Weezer";
   optionC = "5.New Orleans Is Sinking - The Tragically Hip";
   optionD = "2.The Rock Show - Blink 182";
-  optionE = '4.Scar Tissue - Red Hot Chilli Peppers'
-  optionF = '6.Creep - Radio Head'
+  optionE = '4.Scar Tissue - Red Hot Chilli Peppers';
+  optionF = '6.Creep - Radio Head';
   displayMain();
 }
-//haven't done popish links yet
+
+//selection of songs that are made by pop artists
 function displayPopIsh(){
   optionA = "1.Come And Get Your Love - Redbone";
   optionB = "3.Mr. Blue Sky - Electric Light Orchestra";
   optionC = "5.Hit It - Americain Authors";
   optionD = "2.Happier - Marshmello";
-  optionE = '4.Blow - Ed Sheeran'
-  optionF = '6.HeartBeat - Verite'
+  optionE = '4.Blow - Ed Sheeran';
+  optionF = '6.HeartBeat - Verite';
   displayMain();
 }
 
+//I was really just having fun here. enjoy the memes
 function displayMemes(){
   optionA = "1.Mii Channel Theme - Nintendo";
   optionB = "3.Ra Ra Rasputin - Boney M.";
   optionC = "5.Spooky Scary Skeletons - Andrew Gold";
   optionD = "2.Chop Seuy! - System Of A Down";
-  optionE = '4.All Star - Smash Mouth'
-  optionF = '6.Africa - Toto'
+  optionE = '4.All Star - Smash Mouth';
+  optionF = '6.Africa - Toto';
   displayMain();
 }
 
-function displayHistory(){
-  optionA = songHistory[0];
-  optionB = songHistory[1];
-  optionC = songHistory[2];
-  optionD = songHistory[3];
-  optionE = songHistory[4];
-  optionF = songHistory[5];
-  displayMain();
+//making sure if the cosole is open it will go back to normal after it's closed
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
 }

0974f407f8f48b925345cf6cd314408b3f882e3c
LuckEhCanadian <45498441+LuckEhCanadian@users.noreply.github.com>
2019-10-28 14:11:34 -0600

    Game of life implimented

--- Grids demo/sketch.js
+++ Grids demo/sketch.js
@@ -1,71 +1,138 @@
-let grid = [];
-let cols = 30;
+let grid;
 let rows = 30;
+let cols = 30;
+let autoPlay = false;
 
 function setup() {
-  createCanvas(800, 800);
-  grid = make2dArray(cols, rows);
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
+  }
+  else {
+    createCanvas(windowWidth, windowWidth);
+  }
+  grid = createRandom2dArray(cols, rows);
 }
 
 function draw() {
   background(220);
-  displayGrid(grid);
-  fill(random(0,255));
-}
-
-function keyTyped(){
-  if (key === 'r'){
-    
-  }
-  if(key === 'c'){
-    for(let x = 0; x< cols; x++){
-      for (let y = 0; y < rows; y++){
-        grid[y][x] = 1;
-      }
-    }
+  displayGrid(grid, rows, cols);
+  if (autoPlay === true){
+    update();
   }
 }
 
-function mousePressed(){
+function windowResized() {
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
+  }
+  else {
+    createCanvas(windowWidth, windowWidth);
+  }
+}
+
+function keyTyped() {
+  if (key === "r") {
+    grid = createRandom2dArray(cols, rows);
+  }
+  if (key === "c") {
+    grid = createEmptyGrid();
+  }
+  if (key === ' '){
+    update();
+  }
+  if (key === 'a'){
+    autoPlay = !autoPlay;
+  }
+}
+
+function mousePressed() {
   let cellSize = width/cols;
-  let xcoord = floor(mouseX/cellSize);
-  let ycoord = floor(mouseY/cellSize);
-  if (grid[ycoord][xcoord] === 1){
-    grid[ycoord][xcoord] = 0;
+
+  let xCoord = floor(mouseX / cellSize);
+  let yCoord = floor(mouseY / cellSize);
+  
+  if (grid[yCoord][xCoord] === 1) {
+    grid[yCoord][xCoord] = 0;
   }
-  else{
-    grid[ycoord][xcoord] = 1;
+  else {
+    grid[yCoord][xCoord] = 1;
   }
 }
 
-function displayGrid(theGrid){
-  //assumes the grid is a square
-  let cellSize = width/ cols;
-  for(let y = 0; y < rows; y++){
-    for(let x = 0; x< cols; x++){
-      if (theGrid[y][x]===0){
+function displayGrid(grid, rows, cols) {
+  let cellSize = width / cols;
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === 0) {
         fill(255);
       }
-      else{
+      else {
         fill(0);
       }
-      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
     }
   }
 }
 
-function make2dArray(cols, rows){
-  let someArray = [];
-  for(let i = 0; i<cols;i++){
-    someArray.push([]);
-    for (let j = 0; j<rows;j++){
-      if(random(100)<50){
-        someArray[i].push(1);
+function update(){
+  let nextTurn = createEmptyGrid();
+
+  for(let x = 0; x < cols; x++){
+    for(let y = 0; y < rows; y++){
+      let neighbors = 0;
+
+      for (let i = -1; i<=1; i++){
+        for (let j = -1; j<=1; j++){
+          if (x + 1 >=0 && x + i < cols && y+j >= 0 && y+j < rows){
+            neighbors += grid[y+j][x+i];
+          }
+        }
       }
-      else{
-        someArray[i].push(0);
+      neighbors -= grid[y][x];
+      if (grid[y][x] === 1){
+        if (neighbors === 2 || neighbors === 3){
+          nextTurn[y][x] = 1;
+
+        }
+        else{
+          nextTurn[y][x] = 0;
+        }
+      }
+      if (grid[y][x] === 0){
+        if (neighbors === 3){
+          nextTurn[y][x] = 1;
+        }
+        else{
+          nextTurn[y][x] = 0;
+        }
       }
     }
   }
-  return someArray;
+  grid = nextTurn;
+}
+function createEmptyGrid(){
+  let emptyGrid = [];
+  for (let x = 0; x < cols; x++) {
+    emptyGrid.push([]);
+    for(let y = 0; y < rows; y++){
+      emptyGrid[x].push(0);
+    }
+  }
+  return emptyGrid;
+}
+
+function createRandom2dArray(cols, rows) {
+  let randomGrid = [];
+  for (let x = 0; x < cols; x++) {
+    randomGrid.push([]);
+    for (let y = 0; y < rows; y++) {
+      if (random(100) < 50) {
+        randomGrid[x].push(1);
+      }
+      else {
+        randomGrid[x].push(0);
+      }
+    }
+  }
+  return randomGrid;
 }

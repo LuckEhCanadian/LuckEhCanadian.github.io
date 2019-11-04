8dd6e69ae29a768069918ec0924d67a3357b7f30
LuckEhCanadian <45498441+LuckEhCanadian@users.noreply.github.com>
2019-11-04 14:14:06 -0600

    YEEEEEEEEEEEET WALLS

--- portal/sketch.js
+++ portal/sketch.js
@@ -11,8 +11,13 @@ let grid;
 let chamber;
 let playerX = 5;
 let playerY = 5;
+let orangeX;
+let orangeY;
+let blueX;
+let blueY;
 let cellSize;
 let playerPos;
+let portalColor;
 
 
 function setup() {
@@ -23,8 +28,10 @@ function setup() {
   else{
     createCanvas(windowHeight, windowHeight);
   }
+  state = "chamber1"
   grid = createRoom();
   grid[playerX][playerY] = 1;
+  portalColor = 1;
 }
 
 function draw() {
@@ -41,6 +48,12 @@ function displayGrid(grid, rows, cols) {
       else if(grid[y][x] === "wall"){
         fill(125);
       }
+      else if(grid[y][x] === "portalO"){
+        fill(255,119,0);
+      }
+      else if(grid[y][x] === "portalB"){
+        fill(0,177,255);
+      }
       else {
         fill(0);
       }
@@ -52,31 +65,33 @@ function displayGrid(grid, rows, cols) {
 function keyPressed(){
   
   if(key === 'w'){
-    if(playerY > 1){
-      if (grid[playerX][playerY-1] === "wall"){
-        grid[playerY][playerX] = 0;
-        playerY+=1
-      }
-      else{
-        grid[playerY][playerX] = 0;
-        playerY -= 1;
-      }
+    if (grid[playerY-1][playerX] === "wall"){
+    }
+    else{
+      grid[playerY][playerX] = 0;
+      playerY -= 1;
     }
   }
   if (key === 's'){
-    if(playerY < cols-2){
+    if (grid[playerY+1][playerX] === "wall"){
+    }
+    else{
       grid[playerY][playerX] = 0;
       playerY += 1
     }
   }
   if(key === 'd'){
-    if (playerX < cols-2){
+    if (grid[playerY][playerX+1] === "wall"){
+    }
+    else{
       grid[playerY][playerX] = 0;
       playerX += 1
     }
   }
   if(key === 'a'){
-    if (playerX > 1){
+    if (grid[playerY][playerX-1] === "wall"){
+    }
+    else{
       grid[playerY][playerX] = 0;
       playerX -= 1
     }
@@ -95,16 +110,51 @@ function createEmptyGrid(){
   return emptyGrid;
 }
 
+function mousePressed(){
+  let cellSize = width/cols;
+  if(portalColor === 1){
+    orangeY = floor(mouseY / cellSize);
+    orangeX = floor(mouseX / cellSize);
+    if (grid[orangeY][orangeX] != "wall"){
+      grid[orangeY][orangeX] = "portalO";
+      if (portalColor === 0){
+        portalColor += 1
+      }
+      else if(portalColor === 3){
+        grid[orangeY][orangeX] = 0;
+        portalColor -= 1
+      }
+    }
+  }
+  else if(portalColor === 0){
+    grid[blueY][blueX] = 0;
+    blueY = floor(mouseY / cellSize);
+    blueX = floor(mouseX / cellSize);
+    if (grid[blueY][blueX] != "wall"){
+      grid[blueY][blueX] = "portalB";
+      if (portalColor === 1){
+        portalColor += 1
+      }
+      else if (portalColor === 2){
+        grid[orangeY][orangeX] = 0;
+        portalColor += 1
+      }
+    }
+  }
+}
+
 function createRoom(){
   let room = [];
-  for (let x = 0; x < cols; x++){
-    room.push([]);
-    for(let y = 0; y < rows; y++){
-      if (x === 0 || y === 0 || x === cols-1 || y === cols-1 || (y === 10 && x === 5)){
-        room[x].push("wall");
-      }
-      else{
-        room[x].push(0);
+  if (state === "chamber1"){
+    for (let x = 0; x < cols; x++){
+      room.push([]);
+      for(let y = 0; y < rows; y++){
+        if (x === 0 || y === 0 || x === cols-1 || y === cols-1 || (y === 10 && x === 5)){
+          room[x].push("wall");
+        }
+        else{
+          room[x].push(0);
+        }
       }
     }
   }
